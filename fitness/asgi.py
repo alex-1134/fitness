65aaad9bc1459dca3542b4287e4c1e9e30cd582c
alex-1134 <alexa.t.lach@gmail.com>
2022-01-17 19:29:16 +0000

    Set up allauth

--- fitness/asgi.py
+++ fitness/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for fintess project.
+ASGI config for fitness project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fintess.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitness.settings')
 
 application = get_asgi_application()
--- fitness/wsgi.py
+++ fitness/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for fintess project.
+WSGI config for fitness project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fintess.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitness.settings')
 
 application = get_wsgi_application()
