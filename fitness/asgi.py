7fa3cc04f52e603791fe336b08ed3185124804a3
alex-1134 <alexa.t.lach@gmail.com>
2022-01-30 19:00:04 +0000

    Revert "new"
    This reverts commit 38cc4afa3253c1252f5edc89afd69d0d17f5b62a.

--- fitness/asgi.py
+++ fitness/asgi.py
@@ -1,8 +1,10 @@
 """
 ASGI config for fitness project.
+
 It exposes the ASGI callable as a module-level variable named ``application``.
+
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
